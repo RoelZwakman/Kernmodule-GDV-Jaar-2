a2ed3b8ac2af0042c6d788b831f17aa6f4ceb14a
RoelZwakman <31865715+RoelZwakman@users.noreply.github.com>
2017-10-04 18:16:22 +0200

    Update ScoreManager.cs
    BRACKETS ON THE SAME LINE :'(

--- Refactored code/ScoreManager.cs
+++ Refactored code/ScoreManager.cs
@@ -3,13 +3,11 @@
     public static int score; //////The current score
 
 
-    public static void AddScore(int _scoreToAdd = 1)
-    {
+    public static void AddScore(int _scoreToAdd = 1){
         score += _scoreToAdd;
     }
 
-    public static void ResetScore()
-    {
+    public static void ResetScore(){
         score = 0;
     }
 
