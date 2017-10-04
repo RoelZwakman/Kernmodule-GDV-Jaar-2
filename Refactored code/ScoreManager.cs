5f5e2ff5fe0df6285cbce715efd54e217553c46f
RoelZwakman <31865715+RoelZwakman@users.noreply.github.com>
2017-10-04 18:15:38 +0200

    Add files via upload

---
+++ Refactored code/ScoreManager.cs
@@ -0,0 +1,16 @@
+﻿public static class ScoreManager {
+
+    public static int score; //////The current score
+
+
+    public static void AddScore(int _scoreToAdd = 1)
+    {
+        score += _scoreToAdd;
+    }
+
+    public static void ResetScore()
+    {
+        score = 0;
+    }
+
+}
